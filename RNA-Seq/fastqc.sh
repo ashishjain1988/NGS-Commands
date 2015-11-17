050a313fb3513079bdd93ca434f9bb43a1ee180a
Ashish Jain <jain.ashishjain1@gmail.com>
2015-11-17 13:00:29 -0600

    adding fastqc.sh

---
+++ RNA-Seq/fastqc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+folderPath=/home/jain/GEO_Mouse_Datasets/GSE50561_Mouse_TGC_E9.5_Cultured/*.fastq
+#outfolder=/ptmp/jain/Final_Project/Raw_BC_Samples/
+
+for f in $folderPath
+do
+    if [[ -f $f ]]
+    then
+        #echo $f
+        fileName=$(basename $f)
+        out=$(dirname $f)
+        file=(${fileName//./ }) #$(awk -F'.' '{print $1;}' $fileName)
+        #echo $fileName" "$out" "$file[1]
+        #if [ ${file[1]} == 'sra' ]
+        #then
+            echo $f
+            exec="/home/jain/FastQC/fastqc -o $out -f fastq $f &"
+            echo $exec
+            eval $exec
+        #fi
+    fi
+done
+wait
