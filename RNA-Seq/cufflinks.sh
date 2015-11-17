cc78ab889de01505a973a10e750fb77d87541ec0
Ashish Jain <jain.ashishjain1@gmail.com>
2015-11-17 13:10:57 -0600

    updated cufflinks.sh

--- RNA-Seq/cufflinks.sh
+++ RNA-Seq/cufflinks.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 folderPath=/ptmp/jain/Final_Project/Tophat_Output_For_Cufflinks/*
+outFolder=./Cufflink_Output
 
 for f in $folderPath
 do
     if [[ -d $f ]]; then
         fileName=$(basename $f)
 	echo $fileName
-        cufflinks -p 8 -o ./Cufflink_Output/$fileName $f/accepted_hits.bam &
+        cufflinks -p 8 -o $outFolder/$fileName $f/accepted_hits.bam &
     fi
 done
 wait
