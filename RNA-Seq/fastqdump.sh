15b329a3c63ef34329f7d2b97cf481a48d7a1bb9
Ashish Jain <jain.ashishjain1@gmail.com>
2016-01-06 17:10:26 -0600

    Updated for condo

--- RNA-Seq/fastqdump.sh
+++ RNA-Seq/fastqdump.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-folderPath=/ptmp/jain/Final_Project/Raw_BC_Samples/*.sra
-outfolder=/ptmp/jain/Final_Project/Raw_BC_Samples/
+folderPath=/work/LAS/jain/sra/SRR13634*.sra
+outfolder=/work/LAS/Wurtele_Lab/Yeast/Fastq_Files1/
+filePath=/work/LAS/Wurtele_Lab/Yeast/ribosomal_SRR_Accessions.txt
+toolPath=/work/LAS/Wurtele_Lab/sratoolkit.2.5.4-1-ubuntu64/bin
 
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+for i in $(cat "$filePath"); do
+        exec='$toolPath/fastq-dump -Z $i >$outfolder/$i".fastq" &'
+        #exec='$toolPath/prefetch $i &'
+        echo $exec
+        eval $exec
+done
+wait
 
-for f in $folderPath
-do
-    echo $f
-    if [[ -f $f ]] 
-    then
-	fileName=$(basename $f)
+#for f in $folderPath
+#do
+    #echo $f
+    #if [[ -f $f ]]
+    #then
+        #fileName=$(basename $f)
         #echo $fileName
-        file=(${fileName//./ }) #$(awk -F'.' '{print $1;}' $fileName)
+        #file=(${fileName//./ }) #$(awk -F'.' '{print $1;}' $fileName)
         #if [ ${file[1]} = 'sra' ]
         #then
-	    echo $f
-	    fastq-dump -O $outfolder $f &
-	#fi
-    fi
-done
+        #echo $f
+        #$toolPath/fastq-dump -O $outfolder $f &
+        #fi
+   #fi
+#done
 wait
 
