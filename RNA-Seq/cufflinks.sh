9a1e22672b0fdb8cc50464d7e48f2cb15c63a27c
Ashish Jain <jain.ashishjain1@gmail.com>
2016-01-06 17:18:52 -0600

    Updated for condo

--- RNA-Seq/cufflinks.sh
+++ RNA-Seq/cufflinks.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+genomePath=/work/LAS/Wurtele_Lab/Saccharomyces_cerevisiae/Ensembl/EF4/
 folderPath=/ptmp/jain/Final_Project/Tophat_Output_For_Cufflinks/*
 outFolder=./Cufflink_Output
 
 for f in $folderPath
 do
-    if [[ -d $f ]]; then
+   if [[ -d $f ]]; then
         fileName=$(basename $f)
 	echo $fileName
-        cufflinks -p 8 -o $outFolder/$fileName $f/accepted_hits.bam &
+        cufflinks -p 12 -g $genomePath/Annotation/Genes/genes.gtf -o $outFolder/$fileName $f/accepted_hits.bam &
     fi
 done
 wait
